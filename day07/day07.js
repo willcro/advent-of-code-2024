6388610f522a145c1a95d74f53522b7000796179
Will Croteau <willcro@users.noreply.github.com>
2024-12-07 14:55:32 -0600

    Slight optimization to day 7 by minimizing string operations

--- day07/day07.js
+++ day07/day07.js
@@ -3,7 +3,7 @@
 const text = await Deno.readTextFile("./input.txt");
 const lines = text.split("\n");
 
-function toEquation(line, opCount) {
+function toEquation(line) {
   const parts = line.split(": ");
   const result = parts[0] * 1;
   const operands = parts[1].split(" ").map(it => it * 1);
@@ -16,30 +16,31 @@ function solve(equation, opCount) {
   const possibilities = Math.pow(opCount, opNum)
 
   for (let i = 0; i < possibilities; i++) {
-    const operationString = i.toString(opCount).padStart(opNum, "0");
-    if (test(equation, operationString)) {
-      return operationString;
+    if (test(equation, i, opCount)) {
+      return true;
     }
   }
-  return null;
+  return false;
 }
 
-function test(equation, operationString) {
+function test(equation, operationNum, opCount) {
   let accumulator = equation.operands[0];
+  let ops = operationNum;
   for (let i = 1; i < equation.operands.length; i++) {
     if (accumulator > equation.result) {
       return false;
     }
     
     const current = equation.operands[i];
-    const operator = operationString[i - 1];
+    const operator = ops % opCount;
+    ops = (ops - operator) / opCount;
 
-    if (operator == "0") {
+    if (operator == 0) {
       accumulator = accumulator * current;
-    } else if (operator == "1") {
+    } else if (operator == 1) {
       accumulator = accumulator + current;
-    } else if (operator == "2") {
-      accumulator = `${accumulator}${current}` * 1;
+    } else if (operator == 2) {
+      accumulator = accumulator * Math.pow(10, Math.ceil(Math.log10(current + 1))) + current;
     } else {
       throw `Unknown operator ${operator}`;
     }
@@ -48,12 +49,12 @@ function test(equation, operationString) {
   return accumulator == equation.result;
 }
 
-const out = lines.map(toEquation).filter(e => solve(e, 2) != null).reduce((a, b) => a + b.result, 0);
+const out = lines.map(toEquation).filter(e => solve(e, 2)).reduce((a, b) => a + b.result, 0);
 
 console.log(out)
 
 // part 2
 
-const out2 = lines.map(toEquation).filter(e => solve(e, 3) != null).reduce((a, b) => a + b.result, 0);
+const out2 = lines.map(toEquation).filter(e => solve(e, 3)).reduce((a, b) => a + b.result, 0);
 
 console.log(out2)
